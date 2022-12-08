0d54d3c3d5aaeab52e475574f0bbc23d0d4409ea
rai-sahil <raisahil580@gmail.com>
2022-12-07 22:35:43 -0800

    Builder Pattern Created

---
+++ src/main/java/DesignPatterns/BuilderPattern/RobotBuilder.java
@@ -0,0 +1,10 @@
+package DesignPatterns.BuilderPattern;
+
+public interface RobotBuilder {
+
+    public void buildRobotHead();
+    public void buildRobotTorso();
+    public void buildRobotArms();
+    public void buildRobotLegs();
+    public Robot getRobot();
+}
---
+++ src/main/java/DesignPatterns/BuilderPattern/RobotPlan.java
@@ -0,0 +1,9 @@
+package DesignPatterns.BuilderPattern;
+
+public interface RobotPlan {
+
+    public void setRobotHead(String Head);
+    public void setRobotTorso(String Torso);
+    public void setRobotArms(String Arms);
+    public void setRobotLegs(String legs);
+}
